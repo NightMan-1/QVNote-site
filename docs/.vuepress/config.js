0cba5e9b481e825ab30f29781bd2ab1c21bcde05
Sergey Gurinovich <sergey@fsky.info>
2020-01-30 15:28:08 +0300

    better favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: 'QVNote site',
 	head: [
 		['link', { rel: 'icon', href: `/favicon.png` }],
-		['link', { rel: 'apple-touch-icon', href: `/favicon.png` }]
+		['link', { rel: 'apple-touch-icon', href: `/icon.png` }]
 	],
 	themeConfig: {
 		repo: '',
